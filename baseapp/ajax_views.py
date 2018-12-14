5266ee9d64f71dc7957d92f6eb30adf5a88d8108
hongmingu <gmh397@nate.com>
2018-12-14 16:44:18 +0900

    pagepik

--- baseapp/ajax_views.py
+++ baseapp/ajax_views.py
@@ -1282,11 +1282,11 @@ def re_search_all(request):
             url_output = []
             if loc is None:
                 url_objects = UrlObject.objects.filter(Q(urlkeyword__keyword__text__icontains=search_word)).order_by(
-                    'urlkeyword__up_count')[:10]
+                    '-urlkeyword__up_count')[:10]
             else:
                 url_objects = UrlObject.objects.filter(
                     Q(urlkeyword__keyword__text__icontains=search_word) | Q(loc__icontains=loc)).order_by(
-                    'urlkeyword__up_count')[:10]
+                    '-urlkeyword__up_count')[:10]
             for url_object in url_objects:
                 url_output.append(url_object.uuid)
 
@@ -1564,12 +1564,12 @@ def re_search_url(request):
 
             if loc is None:
                 url_objects = UrlObject.objects.filter(Q(urlkeyword__keyword__text__icontains=search_word)).order_by(
-                    'urlkeyword__up_count')[order:order + step]
+                    '-urlkeyword__up_count')[order:order + step]
 
             else:
                 url_objects = UrlObject.objects.filter(
                     Q(urlkeyword__keyword__text__icontains=search_word)
-                    | Q(loc__icontains=loc)).order_by('urlkeyword__up_count')[order:order + step]
+                    | Q(loc__icontains=loc)).order_by('-urlkeyword__up_count')[order:order + step]
 
             end = 'false'
             if url_objects.count() < step:
